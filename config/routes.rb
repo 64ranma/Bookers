cc21f4bb6d1a9e3f0ac8526326e7a60df0c139e1
64ranma <125184575+64ranma@users.noreply.github.com>
2024-03-05 04:47:08 +0000

    [add]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/top' => 'homes#top'
   get 'books/new'
+  post 'books' => 'books#create'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
