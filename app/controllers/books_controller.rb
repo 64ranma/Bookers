9b4954c8c815f08b18485aa79b9aec0792ea851b
64ranma <125184575+64ranma@users.noreply.github.com>
2024-03-07 01:37:41 +0000

    [start]3/7

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,14 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    redirect_to '/'
   end
-  
-  
+
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
